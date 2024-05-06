ecef9849e54a3018008d25213242b45e406ebf02
Tony Ortega <antonio.ortega@3xmgroup.com>
2024-05-06 19:54:11 +0200

    cleaning up more

--- todo-code-sample/src/components/TodoList.tsx
+++ todo-code-sample/src/components/TodoList.tsx
@@ -1,5 +1,3 @@
-// TodoList.tsx
-
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../store/store';
--- todo-code-sample/src/store/store.ts
+++ todo-code-sample/src/store/store.ts
@@ -1,5 +1,3 @@
-// store.ts
-
 import { createStore } from 'redux';
 import rootReducer from '../reducers/reducers';
 
